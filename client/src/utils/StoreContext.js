92426888b9de5ed1ddac8626cb36c086d25f52f7
Bailey Staub <37120658+baystaub@users.noreply.github.com>
2021-07-22 19:14:51 -0600

    Delete client directory

--- client/src/utils/StoreContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-
-import store from './store';
-
-export default function StoreProvider(props) {
-    // Return the provider component provided by react-redux
-    return <Provider store={store} {...props} />;
-}
