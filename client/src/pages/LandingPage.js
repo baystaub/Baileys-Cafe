1e564d8914fcce0e6dbe3b386897489a077495b3
K Ryan <hunt.kelsearyan@gmail.com>
2021-07-25 14:56:22 -0600

    Copied single files over and npm watchpack as it was broken in the packagelock

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../components/LandingPage";
+
+const LandingPage = () => {
+  return (
+    <div className="container">
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -6,11 +6,6 @@ const NoMatch = () => {
     <div>
       <Jumbotron>
         <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
       </Jumbotron>
     </div>
   );
