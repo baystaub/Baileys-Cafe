4c5aac1f07d67935b69f25df3b9fa029da372d12
K Ryan <hunt.kelsearyan@gmail.com>
2021-07-29 20:11:24 -0600

    Updated Landing Page to say Order History, Menu, Contact instead if you are logged in versus Login, Menu. Contact

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,19 +1,33 @@
 import React from "react";
+import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-import '../../landing.css';
+import "../../landing.css";
 import cafe from "../../assets/cafe.png";
 
 const LandingPage = () => {
-  return (
-    <div className="landingHome">
-      <img src={cafe} alt="Cafe" className="landing"/>
-    <footer className="footer">
-      <Link to="/login">SIGN IN</Link>
-      <Link to="/home">MENU</Link>
-      <a href="mailto:manager@email.com">CONTACT</a>
-    </footer>
-    </div>
-  );
+  if (Auth.loggedIn()) {
+    return (
+      <div className="landingHome">
+        <img src={cafe} alt="Cafe" className="landing" />
+        <footer className="footer">
+          <Link to="/orderhistory">ORDER HISTORY</Link>
+          <Link to="/home">MENU</Link>
+          <a href="mailto:manager@email.com">CONTACT</a>
+        </footer>
+      </div>
+    );
+  } else {
+    return (
+      <div className="landingHome">
+        <img src={cafe} alt="Cafe" className="landing" />
+        <footer className="footer">
+          <Link to="/login">LOGIN</Link>
+          <Link to="/home">MENU</Link>
+          <a href="mailto:manager@email.com">CONTACT</a>
+        </footer>
+      </div>
+    );
+  }
 };
 
 export default LandingPage;
